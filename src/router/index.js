c05654781b012869e26d4d65993c6d42d844839f
Aha-L446 <1716166071@qq.com>
2022-08-16 09:55:48 +0800

    复用views

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
-import Vue from "vue"
-import Router from "vue-router"
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const home = () => import('views/home/Home')
+const category = () => import('views/category/Category')
+const cart = () => import('views/cart/Cart')
+const profile = () => import('views/profile/Profile')
 
 Vue.use(Router)
 
@@ -7,6 +12,26 @@ const router = new Router({
   routes: [{
     path: '/',
     redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: home
+  },
+  {
+    path: '/category',
+    name: 'category',
+    component: category
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: cart
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: profile
   }],
   mode: 'history'
 })
